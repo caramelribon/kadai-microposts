772375b3eb0a496e419b0581d719228dd0c1d3dd
ribon.caramel <caramelribon137@gmail.com>
2021-10-21 07:46:36 +0000

    dependent: :destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   # micropost側からfavoriteに参照
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   
   validates :content, presence: true, length: { maximum: 255 }
 end
