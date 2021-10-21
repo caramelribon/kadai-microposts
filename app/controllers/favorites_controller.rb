3aa38e514f9090c5ceacf84eb58bde8201746633
ribon.caramel <caramelribon137@gmail.com>
2021-10-21 12:22:09 +0000

    favorite button fix

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = 'お気に入りにしました。'
-    redirect_back fallback_location: root_path
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    micropost = current_user.favorites.find_by(params[:id]).micropost
+    micropost = Micropost.find(params[:micropost_id])
     current_user.unfavorite(micropost)
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_back fallback_location: root_path
+    redirect_back(fallback_location: root_path)
   end
 end
